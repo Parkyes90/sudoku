2995270e24887ae6ea7f49db28c11a891a597604
parkyes90 <parkyes90@gmail.com>
2020-05-24 18:36:10 +0900

    add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { unregister } from 'core';
-
+import { GlobalStyles } from './styles';
 ReactDOM.render(
   <React.StrictMode>
-    <div> Hello world</div>
+    <>
+      <GlobalStyles />
+      <div>Hello world</div>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
